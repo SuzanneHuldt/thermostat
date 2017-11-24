49b355752eb9aee031a1939df87643fe41eefafa
Suzanne Huldt <suzanne.huldt.16@ucl.ac.uk>
2017-11-24 14:35:06 +0000

    now with full jquery functionality :)

--- views/interface.js
+++ views/interface.js
@@ -1,21 +1,46 @@
 
   $(document).ready(function() {
     $("#Off").hide();
-    
+    $(".usage").hide();
     var thermostat = new Thermostat
+
+    if(thermostat.energyUsage() === 'low-usage') {
+      $("#low_usage").show();
+    } else if (thermostat.energyUsage() === 'medium-usage'){
+      $("#medium_usage").show();
+    } else {
+      $("#high_usage").show();
+
+    }
 
     $(".buttonUP").click(function() {
       thermostat.up()
       $(".temp").text(thermostat.temperature)
+
       alert( "Temperature will be raised by 1 degree" );
-      console.log(thermostat.temperature)
+      if(thermostat.energyUsage() === 'low-usage') {
+        low();
+      } else if (thermostat.energyUsage() === 'medium-usage'){
+        medium();
+      } else {
+        high();
+
+      }
+    
     });
 
     $(".buttonDOWN").click(function() {
       thermostat.down()
       $(".temp").text(thermostat.temperature)
       alert("Temperature will be lowered by 1 degree");
-      console.log(thermostat.temperature)
+      if(thermostat.energyUsage() === 'low-usage') {
+        low();
+      } else if (thermostat.energyUsage() === 'medium-usage'){
+        medium();
+      } else {
+        high();
+
+      }
     });
 
     $(".buttonSAVE").click(function() {
@@ -32,4 +57,22 @@
 
     });
 
+
+    high = function() {
+      $("#high_usage").show();
+      $("#medium_usage").hide();
+      $("#low_usage").hide();
+    }
+
+    medium = function() {
+      $("#high_usage").hide();
+      $("#medium_usage").show();
+      $("#low_usage").hide();
+    }
+
+    low = function() {
+      $("#high_usage").hide();
+      $("#medium_usage").hide();
+      $("#low_usage").show();
+    }
   });
